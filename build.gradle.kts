ec5b732869a3dae94b8c9a56528e916eb8725527
Leon Heuer <leon.heuer@outlook.de>
2022-03-05 18:16:31 +0100

    Update GUI API

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ dependencies {
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
     compileOnly("com.github.SkriptLang:Skript:2.5.3")
     compileOnly("net.luckperms:api:5.3")
-    implementation("com.github.heuerleon:mcguiapi:v1.3.1")
+    implementation("com.github.heuerleon:mcguiapi:v1.3.2")
 }
 
 tasks {
@@ -43,6 +43,10 @@ tasks {
             exclude(dependency("net.luckperms:api:5.3"))
             exclude(dependency("com.github.SkriptLang:Skript:2.5.3"))
         }
+    }
+
+    build {
+        dependsOn(shadowJar)
     }
 }
 
